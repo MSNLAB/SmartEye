5d81ef07614c835d28b28b1c61a3553b18523d7a
ZhiyanWXZ <1050642597@qq.com>
2021-04-10 17:43:43 +0800

    virtual camera

--- virtual_camera.py
+++ virtual_camera.py
@@ -16,9 +16,9 @@ from PIL import Image
 import subprocess
 import os
 import json
+import time
 
 
-subprocess.Popen(['sudo', 'modprobe', 'v4l2loopback', 'devices=1'], stdout=subprocess.PIPE)
 video_path = '/home/wxz/Desktop/20200827153531.mp4'
 
 
@@ -28,9 +28,11 @@ def virtual_camera(video_name):
     :param video_name: video sent to virtual camera
     :return:
     """
-
+    p = subprocess.call(['sudo', 'modprobe', 'v4l2loopback', 'devices=1'], stdout=subprocess.PIPE)
+    # p.communicate()
     cap = cv2.VideoCapture(video_name)
     width, height, fps = get_video_info(video_name)
+    # print(width, height, fps)
     with pyvirtualcam.Camera(width=width, height=height, fps=fps) as cam:
 
         while cap.isOpened():
@@ -53,20 +55,27 @@ def get_video_info(video_name):
     :param video_name: video
     :return:
     """
+    # /home/wxz/Documents/video2edge
     name = os.path.basename(video_name).split(".")[0]
     json_file = "./" + name + ".json"
+    # Does the reason that add the sudo command in the front result in program stop?
     cmd = ("ffprobe -print_format json -show_format -show_streams -i " +
-           video_name + ">>" + json_file)
-    print(cmd)
-    subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-    with open(json_file, 'r') as f:
+           video_name + " > " + json_file)
+    # subprocess.Popen create a subprocess to execute the cmd,
+    # and the parent process don't wait the subprocess to finish
+    # unless you use the wait or communicate function
+    subprocess.call(cmd, shell=True, stderr=subprocess.PIPE)
+
+    with open(json_file, 'r', encoding='utf8') as f:
+        # info = f.read()
+        # print(info)
         info = json.load(f)
-        width = info["streams"]["width"]
-        height = info["streams"]["height"]
-        frames = info["streams"]["r_frame_rate"]
-    # out = info.communicate()
-    # print(out)
+    width = info["streams"][0]["width"]
+    height = info["streams"][0]["height"]
+    fps = info["streams"][0]["r_frame_rate"]
+    return int(width), int(height), eval(fps)
 
 
 if __name__ == "__main__":
+    # virtual_camera(video_path)
     get_video_info(video_path)
