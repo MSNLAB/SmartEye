cd45935257898eb87231c692c0da7e10f054144b
ZhiyanWXZ <1050642597@qq.com>
2021-06-16 20:24:54 +0800

    new

--- client.py
+++ client.py
@@ -55,34 +55,34 @@ def offload_worker(serv_type, offload_queue, msg_dict, sys_info, local_store):
     # logger.debug("mark")
     preprocessor = PreProcessor()
     image_url = read_config("transfer-url", "image_url")
-    while True:
-        frame = offload_queue.get()
-        # logger.debug("msg_dict:"+str(id(msg_dict)))
 
-        frame = preprocessor.preprocess_image(frame, **msg_dict)
-        file_size = sys.getsizeof(frame)
-        # send the video frame to the server
-        try:
-            result_dict, start_time, processing_delay, arrive_transfer_server_time = \
-                send_frame(image_url, frame, selected_model)
-        except Exception as err:
-            logger.exception("return back err!")
+    frame = offload_queue.get()
+    # logger.debug("msg_dict:"+str(id(msg_dict)))
+
+    frame = preprocessor.preprocess_image(frame, **msg_dict)
+    file_size = sys.getsizeof(frame)
+    # send the video frame to the server
+    try:
+        result_dict, start_time, processing_delay, arrive_transfer_server_time = \
+            send_frame(image_url, frame, selected_model)
+    except Exception as err:
+        logger.exception("return back err!")
+    else:
+        bandwidth = file_size / arrive_transfer_server_time
+        if serv_type == common.IMAGE_CLASSIFICATION:
+            result = result_dict["prediction"]
+            sys_info.append(start_time, processing_delay, bandwidth)
+            logger.info("offload:"+result)
+
+        elif serv_type == common.OBJECT_DETECTION:
+            frame_shape = tuple(int(s) for s in result_dict["frame_shape"][1:-1].split(","))
+            frame_handled = transfer_array_and_str(result_dict["result"], 'down').reshape(frame_shape)
+            local_store.store_image(frame_handled)
+            sys_info.append(start_time, processing_delay, bandwidth)
+            logger.info("offload object detection works well!")
+
         else:
-            bandwidth = file_size / arrive_transfer_server_time
-            if serv_type == common.IMAGE_CLASSIFICATION:
-                result = result_dict["prediction"]
-                sys_info.append(start_time, processing_delay, bandwidth)
-                logger.info("offload:"+result)
-
-            elif serv_type == common.OBJECT_DETECTION:
-                frame_shape = tuple(int(s) for s in result_dict["frame_shape"][1:-1].split(","))
-                frame_handled = transfer_array_and_str(result_dict["result"], 'down').reshape(frame_shape)
-                local_store.store_image(frame_handled)
-                sys_info.append(start_time, processing_delay, bandwidth)
-                logger.info("offload object detection works well!")
-
-            else:
-                logger.error("Seal error: no specified service type!")
+            logger.error("Seal error: no specified service type!")
 
 
 if __name__ == '__main__':
