ac93ed0c940bd1a4f074480ee1726ec2cd92860a
Guanyu Gao <guanyugao@Guanyus-MacBook-Pro.local>
2021-06-19 16:47:03 +0800

    implment some basic control policy

--- local/decision_engine.py
+++ local/decision_engine.py
@@ -1,42 +1,51 @@
-import random
 import edge_globals
-from tools.read_config import read_config
-from loguru import logger
+from model_manager.model_cache import edge_object_detection_model
+from model_manager.model_cache import cloud_object_detection_model
+from model_manager.model_cache import get_fastest_model
+
+resolution_list = [(240, 352), (360, 480), (480, 858), (720, 1280)]
+qp_value = [30, 40, 50, 60, 70, 80, 90]
+
+
+# the video frame will be always processed on edge
+def always_local_fastest_model(task):
+    task.location = edge_globals.LOCAL
+    if task.serv_type == edge_globals.OBJECT_DETECTION:
+        task.selected_model = get_fastest_model(edge_object_detection_model)
+    return task
+
+
+# the video frame will be always processed on the cloud
+def always_cloud_lowest_delay(task):
+    task.location = edge_globals.OFFLOAD
+    task.new_size = resolution_list[0]
+    if task.serv_type == edge_globals.OBJECT_DETECTION:
+        task.selected_model = get_fastest_model(cloud_object_detection_model)
+    return task
+
+
+def threshold_offload_policy(task):
+    pass
+
+
+def delay_precision_tradeoff(task):
+    pass
 
 
 class DecisionEngine:
     def __init__(self, sys_info):
-        self.object_detection_models = read_config("object-detection")
-        self.image_classification_models = read_config("image-classification")
-        self.resolution_list = []
-        self.qp_value = 0
         self.sys_info = sys_info
-        self.policy_set = {"always_local_fastest_model": self.always_local_fastest_model,
-                           "always_cloud_lowest_delay":  self.always_cloud_lowest_delay}
+        self.policy_set = {"always_local_fastest_model": always_local_fastest_model,
+                           "always_cloud_lowest_delay": always_cloud_lowest_delay}
+        self.last_frame = None
 
+    def get_decision(self, policy, task):
+        # make decision based on the policy
+        task = self.policy_set[policy](task)
+        # save the frame for differencing in the next time
+        self.last_frame = task.frame
+        return task
 
-    # the video frame will be always processed on edge
-    def always_local_fastest_model(self):
-        pro_location = edge_globals.LOCAL
-        pre_proc = None
-        selected_model = None
-        return pro_location, pre_proc, selected_model
-
-    # the video frame will be always processed on the cloud
-    def always_cloud_lowest_delay(self):
-        pro_location = edge_globals.OFFLOAD
-        pre_proc = None
-        selected_model = None
-        return pro_location, pre_proc, selected_model
-
-    def threshold_offload_policy(self):
-        pass
-
-    def delay_precision_tradeoff(self):
-        pass
-
-    def get_decision(self, policy):
-        return self.policy_set[policy]()
 
 
 
